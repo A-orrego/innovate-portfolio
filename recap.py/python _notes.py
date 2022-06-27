816138b0eabe5a4348f6ded3000ddeddcd16c3d6
A-orrego <ammatullahorrego@gmail.com>
2022-06-27 17:15:11 +0100

    added notes about data tyers

--- recap.py/python _notes.py
+++ recap.py/python _notes.py
@@ -1,1 +1,15 @@
-print("hello world")
+# this is how to show info you  dont want to show in the terminal.
+
+print("Recap of python learnt in LVL 2 course")
+
+greeting = "hello world"
+
+print(greeting)
+
+# basic data types
+print("this is a string displaying characters") #this is a string
+print(1234+1)#this is an interger/whole number
+print(12.34) #this is a floating number
+print(True) #one of the boolean data type
+print(False) #one of the booleans
+print(None) #none - black/null data
